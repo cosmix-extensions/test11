1bffc7c93edf838c7a1879c99e977bc9ed650ad2
Ayon <senkuboy0@gmail.com>
2026-08-08 14:07:02 +0600

    fix(plugin): Use Plugin() instead of BasePlugin() to match working reference

--- HanimeProvider/src/main/kotlin/com/hanime/HanimePlugin.kt
+++ HanimeProvider/src/main/kotlin/com/hanime/HanimePlugin.kt
@@ -1,10 +1,11 @@
 package com.hanime
 
-import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
 
 @CloudstreamPlugin
-class HanimePlugin: BasePlugin() {
+class HanimePlugin: Plugin() {
     override fun load() {
         registerMainAPI(HanimeProvider())
     }
